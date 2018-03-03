51b6f6f419d822409a4e4d3d20adff80cca169ee
Shane Vitarana <shanev@gmail.com>
2018-03-03 00:23:12 -0500

    Added ArtistToken and test skeleton

---
+++ contracts/ArtistToken.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+import "zeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
+
+
+/**
+ * @title Artist token
+ * @dev Mintable token with a token cap.
+ */
+contract ArtistToken is CappedToken {
+
+  string public constant name = "Tiga Coin"; // solium-disable-line uppercase
+  string public constant symbol = "TIGA"; // solium-disable-line uppercase
+  uint8 public constant decimals = 18; // solium-disable-line uppercase
+
+  uint256 public cap = 10000000 * (10 ** uint256(decimals));
+}
--- truffle.js
+++ truffle.js
@@ -1,4 +1,15 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    local: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
+    kovan: {
+      host: '198.199.66.129',
+      port: 8545,
+      network_id: '*',
+      gas: 6431652,
+    },
+  },
 };
